c091a6404f9159ca778b620a3cfc578dee1ada7b
Liu Yahui <liuyahui@jfpal.com>
2017-03-27 21:55:49 +0800

    fix change apis

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -12,13 +12,17 @@
               <img class="images-con imgpic" v-bind:src="(config.api+A.img_group[0].photopath)" >
             </div>
             <div class="box-flex flex-1 padding-all flex-direction-column">
+                <router-link :to="('/detail/'+A._id)">
                 <span class="font-size-16 lineThrou">{{A.tirtle}}</span>
+                </router-link>
                 <span class="font-size-12">{{A.info}}</span>
             </div>
            </div>
            <div class="box-flex width-100" v-else> 
             <div class="box-flex flex-1 padding-all flex-direction-column">
-                <span class="font-size-16 lineThrou">{{A.tirtle}}</span>
+                <router-link :to="('/detail/'+A._id)">
+                  <span class="font-size-16 lineThrou">{{A.tirtle}}</span>
+                </router-link>
                 <span class="font-size-12">{{A.info}}</span>
             </div>
             <div class="flex-1">
@@ -95,10 +99,14 @@
         <div class="line-height-30 font-size-12 textclolor-black-low">© 2015 All rights reserved. Design and development by Liu Yahui</div>
         <div class="line-height-30 font-size-12 textclolor-white ">Projects made with </div>
         <div class="width-80 margin-auto text-align-center flex-justify-center flex-items-center flex-content-center">
+            <a href="https://github.com/liuyahuiZ">
             <span class="flex-1 ion-social-github font-size-20 textclolor-white margin-right-3"></span>
+            </a>
             <span class="flex-1 ion-social-twitter font-size-20 textclolor-white margin-right-3"></span>
             <span class="flex-1 ion-social-facebook font-size-20 textclolor-white margin-right-3"></span>
+            <a href="Mailto:liuyahui991@gmail.com">
             <span class="flex-1 ion-social-googleplus font-size-20 textclolor-white margin-right-3"></span>
+            </a>
             <span class="flex-1 ion-social-dribbble-outline font-size-20 textclolor-white margin-right-3"></span>
         </div>
     </div>
