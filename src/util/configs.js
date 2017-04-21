90e940570870687ccfb47c54baa71d769200bcf0
Liu Yahui <liuyahui@jfpal.com>
2017-04-21 16:24:22 +0800

    fix change

--- src/util/configs.js
+++ src/util/configs.js
@@ -1,3 +1,3 @@
-let config = {'api': 'http://localhost:2016/getphotoPal/'}
+let config = {'api': 'http://localhost:2016/getphotoPal/','imgurl':'http://localhost:2016/readfPhoto/'}
 // http://47.88.2.72:2016
 export default { config }
