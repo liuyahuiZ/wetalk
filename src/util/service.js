edf1952c54c4d7b4ad47f65b7813d9b8c282ca30
Liu Yahui <liuyahui@jfpal.com>
2017-05-05 15:21:23 +0800

    fix change src

--- src/util/service.js
+++ src/util/service.js
@@ -1,5 +1,21 @@
 import '@/util/jquery.js'
 class Service {
+  constructor(){
+    Promise.prototype.finally = function (callback) {
+    let P = this.constructor;
+      return this.then(
+        value  => P.resolve(callback()).then(() => value),
+        reason => P.resolve(callback()).then(() => { throw reason })
+      );
+    };
+    Promise.prototype.done = function (onFulfilled, onRejected) {
+      this.then(onFulfilled, onRejected)
+        .catch(function (reason) {
+          // 抛出一个全局错误
+          setTimeout(() => { throw reason }, 0);
+        });
+    };
+  }
   Get(api,reqbody){
     console.log(api,reqbody)
     return new Promise(function (resolve, reject) {
